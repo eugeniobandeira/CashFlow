e2b5293206aae42784826baa50b51226e8ddb169
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2024-09-12 22:08:06 -0300

    feat: improving business logic

--- CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,13 +1,34 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register
 {
     public class RegisterExpenseUseCase
     {
-        public RegisteredExpenseResponse Execute(InsertExpenseRequest req)
+        public static RegisteredExpenseResponse Execute(InsertExpenseRequest req)
         {
+            Validate(req);
+
             return new RegisteredExpenseResponse();
+        }
+
+        private static void Validate(InsertExpenseRequest req) 
+        { 
+            var isTitleEmpty = string.IsNullOrWhiteSpace(req.Title);
+            if (isTitleEmpty)
+                throw new ArgumentException("The title can not be null or whitespace");
+
+            if (req.Amount <= 0)
+                throw new ArgumentException("The amount must be higher than 0");
+
+            var result = DateTime.Compare(req.Date, DateTime.UtcNow);
+            if (result > 0)
+                throw new ArgumentException("It is not allowed to use future date");
+
+            var isPaymentTypeValid = Enum.IsDefined(typeof(PaymentType), req.Type);
+            if (!isPaymentTypeValid)
+                throw new ArgumentException("Payment type not valid");
         }
     }
 }
