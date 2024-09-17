d332ec27bffa33383468bdf42d9d77ae71c126a2
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2024-09-17 20:00:08 -0300

    feat: connection string (context)

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess
+{
+    public class CashFlowDbContext : DbContext
+    {
+        public DbSet<ExpenseEntity> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseOracle("User Id=RM550882;Password=06082000;Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle.fiap.com.br)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ORCL)))");
+        }
+    }
+}
