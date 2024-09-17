7eb1735f69d58c27daf48edf2dbe1a444dbbdc46
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2024-09-17 13:30:43 -0300

    feat: ExpenseEntity

---
+++ src/ClashFlow.Domain/Entities/ExpenseEntity.cs
@@ -0,0 +1,14 @@
+﻿using CashFlow.Domain.Enums;
+
+namespace CashFlow.Domain.Entities
+{
+    public class ExpenseEntity
+    {
+        public long ID_EXPENSE {  get; set; }
+        public string CD_TITLE { get; set; } = string.Empty;
+        public string? DS_DESCRIPTION { get; set; }
+        public DateTime DT_DATE { get; set; }
+        public decimal VL_AMOUNT { get; set; }
+        public PaymentTypeEnum TP_PAYMENT_TYPE { get; set; }
+    }
+}
---
+++ src/ClashFlow.Domain/Enums/PaymentTypeEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace CashFlow.Domain.Enums
+{
+    public enum PaymentTypeEnum
+    {
+        Cash = 0,
+        CreditCard = 1,
+        DebitCard = 2,
+        EletronicTransfer = 3
+    }
+}
