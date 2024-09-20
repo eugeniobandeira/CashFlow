b274e2dab8d9f7e7340294605a7cd4f3533da3c7
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2024-09-19 21:02:49 -0300

    feat: adding AutoMapper

---
+++ src/CashFlow.Application/AutoMapper/AutoMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CashFlow.Communication.Requests;
+using CashFlow.Domain.Entities;
+
+namespace CashFlow.Application.AutoMapper
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            RequestToEntity();
+            EntityToResponse();
+        }
+
+        private void RequestToEntity() 
+            => CreateMap<InsertExpenseRequest, ExpenseEntity>();
+
+        private void EntityToResponse() 
+            => CreateMap<ExpenseEntity, InsertExpenseRequest>();
+    }
+}
