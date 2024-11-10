feabd801d66e351bd7ad08f3aeef06d45ce575c7
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2024-11-10 20:39:51 -0300

    test: WebApplicationFactory (still saving user in database)

--- tests/WebApi.Test/Users/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -1,13 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
 
 namespace WebApi.Test.Users.Register
 {
     public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
     {
+        private const string METHOD = "v1/api/user";
+
+        private readonly HttpClient _httpClient;
+
+        public RegisterUserTest(WebApplicationFactory<Program> webApplicationFactory)
+        {
+            _httpClient = webApplicationFactory.CreateClient();
+        }
+
         [Fact]
         public async Task Success()
         {
+            //Arrange
+            var req = InsertUserRequestBuilder.Build();
 
+            //Act
+            var result = await _httpClient.PostAsJsonAsync(METHOD, req);
+
+            //Assert
+            result.StatusCode.Should().Be(HttpStatusCode.Created);
         }
     }
 }
