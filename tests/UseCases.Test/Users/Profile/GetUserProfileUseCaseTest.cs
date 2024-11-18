b3609746fb1c143c867fd34326cc7ff4a1500542
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2024-11-18 20:33:10 -0300

    test: get user profile use case and integration test

---
+++ tests/UseCases.Test/Users/Profile/GetUserProfileUseCaseTest.cs
@@ -0,0 +1,36 @@
+﻿using CashFlow.Application.UseCases.Users.Profile;
+using CashFlow.Domain.Entities;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Mapper;
+using FluentAssertions;
+
+namespace UseCases.Test.Users.Profile
+{
+    public class GetUserProfileUseCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            //Arrange
+            var user = UserBuilder.Build();
+            var useCase = CreateUseCase(user);
+
+            //Act
+            var result = await useCase.Execute();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Name.Should().Be(user.Name);
+            result.Email.Should().Be(user.Email);
+        }
+
+        private static GetUserProfileUseCase CreateUseCase(UserEntity user)
+        {
+            var mapper = MapperBuilder.Build();
+            var loggedUser = LoggedUserBuilder.Build(user);
+
+            return new GetUserProfileUseCase(loggedUser, mapper);
+        }
+    }
+}
---
+++ tests/WebApi.Test/Users/Profile/GetUserProfileTest.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using System.Net;
+using System.Text.Json;
+
+namespace WebApi.Test.Users.Profile
+{
+    public class GetUserProfileTest : CashflowClassFixture
+    {
+        private const string METHOD = "v1/api/user";
+
+        private readonly string _token;
+        private readonly string _userName;
+        private readonly string _userEmail;
+
+        public GetUserProfileTest(IntegrationTestWebApplicationFactory webAppFactory) : base(webAppFactory)
+        {
+            _token = webAppFactory.Regular_User_Manager.GetToken();
+            _userName = webAppFactory.Regular_User_Manager.GetName();
+            _userEmail = webAppFactory.Regular_User_Manager.GetEmail();
+        }
+
+        [Fact]
+        public async Task Success()
+        {
+            //Act
+            var result = await DoGetAsync(reqUri: METHOD, token: _token);
+
+            //Assert
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var body = await result.Content.ReadAsStreamAsync();
+            var response = await JsonDocument.ParseAsync(body);
+
+            response.RootElement.GetProperty("name").GetString().Should().Be(_userName);
+            response.RootElement.GetProperty("email").GetString().Should().Be(_userEmail);
+        }
+
+    }
+}
